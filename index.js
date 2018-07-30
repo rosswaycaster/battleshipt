b93b2adcc167dccb2470ea46f7c2f2f1468e6824
Ross Waycaster <rosswaycaster@me.com>
2018-07-30 14:54:30 -0500

    add prompt function

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const bs = require("./battleshipt");
+const readline = require("readline");
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+const prompt = q => {
+  return new Promise(resolve => {
+    rl.question(q + "\n", answer => {
+      if (answer) {
+        resolve(answer);
+      }
+    });
+  });
+};
 
 let state = bs.initialState();
 
 const gameLoop = () => {
-  //check for winner
+  //check for a winning player
+  const winningPlayer = bs.hasWinner(state);
+  if (winningPlayer) {
+    console.log(`Player ${winningPlayer} Wins!`);
+  }
+
+  return gameLoop();
 };
 
 gameLoop();
